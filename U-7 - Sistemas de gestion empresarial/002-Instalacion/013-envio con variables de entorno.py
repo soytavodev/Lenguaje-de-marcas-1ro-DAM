b3a40a8df89c5cbab899053a3029787c852fa467
Gustavo Delnardo <gustavoedv35@gmail.com>
2026-01-26 12:38:27 +0100

    1

--- U-7 - Sistemas de gestion empresarial/002-Instalacion/013-envio con variables de entorno.py
+++ U-7 - Sistemas de gestion empresarial/002-Instalacion/013-envio con variables de entorno.py
@@ -2,21 +2,23 @@ import smtplib
 from email.message import EmailMessage
 import os
 
-SMTP_SERVER = os.environ.get("EMAIL_SERVIDOR")
-SMTP_PORT = 587          # 587 = STARTTLS, 465 = SSL
-SMTP_USER = os.environ.get("EMAIL_USUARIO")
-SMTP_PASS = os.environ.get("EMAIL_CONTRASENA")
+SMTP_SERVER = "smtp.resend.com"
+SMTP_PORT = 465
+SMTP_USER = "resend"
+
+SMTP_PASS = os.environ.get("RESEND_API_KEY")
+
+if not SMTP_PASS:
+    raise ValueError("La variable de entorno 'RESEND_API_KEY' no está definida o está vacía.")
 
 msg = EmailMessage()
-msg["From"] = "nopis35607@gxuzi.com"
+msg["From"] = "onboarding@resend.dev"
 msg["To"] = "gustavoedv35@gmail.com"
 msg["Subject"] = "Esto es un ejercicio de clase"
-msg.set_content("Hola esto es una prueba desde Python.\n")
+msg.set_content("¡Por fin carajo!, Hola, esto es una prueba desde Python.\n.")
 
-with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
-    smtp.starttls()
+with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp:
     smtp.login(SMTP_USER, SMTP_PASS)
     smtp.send_message(msg)
 
-print("Email sent")
-
+print("Email enviado correctamente.")
